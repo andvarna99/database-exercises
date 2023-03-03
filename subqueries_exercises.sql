d02f68eaf1c88e3d26619e74c61f7103290ce291
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-03-03 16:22:06 -0600

    3/3 exercises

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,30 +1,39 @@
 use ymir_employees;
 
-SELECT e.hire_date, e.first_name, e.last_name
+SELECT *
 FROM employees AS e
 WHERE e.hire_date IN (
-    SELECT e.hire_date
-    FROM employees
-    WHERE e.hire_date = '1990-10-22'
+    SELECT e2.hire_date
+    FROM employees AS e2
+    WHERE e2.emp_no = '101010'
     );
 
 SELECT t.title, e.first_name, e.last_name
 FROM employees AS e
     JOIN titles AS t ON t.emp_no = e.emp_no
     WHERE e.first_name IN (
-        SELECT e.first_name
-        FROM employees
-        WHERE e.first_name = 'Aamod'
+        SELECT e2.first_name
+        FROM employees AS e2
+        WHERE e2.first_name = 'Aamod'
         );
 
-SELECT e.first_name, e.last_name, e.gender
+SELECT distinct t.title
 FROM employees AS e
-    JOIN dept_manager AS dm ON dm.emp_no = e.emp_no
-    WHERE e.gender IN (
-        SELECT e.gender
-        FROM employees
-        WHERE e.gender = 'F'
-        ) AND dm.to_date = '9999-01-01';
+         JOIN titles AS t ON t.emp_no = e.emp_no
+WHERE e.first_name IN (
+    SELECT e2.first_name
+    FROM employees AS e2
+    WHERE e2.first_name = 'Aamod'
+);
+
+SELECT e.first_name, e.last_name
+FROM employees AS e
+WHERE e.gender = 'F'
+AND e.emp_no IN (
+        SELECT dm2.emp_no
+        FROM dept_manager AS dm2
+        WHERE dm2.to_date = '9999-01-01'
+        );
 
 SELECT d.dept_name AS Department
 FROM departments AS d
@@ -38,9 +47,9 @@ WHERE d.dept_no IN (
 
 SELECT e.first_name, e.last_name
 FROM employees AS e
-WHERE e.emp_no IN (
+WHERE e.emp_no = (
     SELECT s.emp_no
     FROM salaries AS s
-    ORDER BY s.salary
+    ORDER BY s.salary DESC
     LIMIT 1
     );
